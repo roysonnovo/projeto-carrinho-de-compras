9d3a0ccddd6fa0b3c2df6fe02fac8c274dba1188
roysonnovo <royson.novo@gmail.com>
2025-09-03 09:44:32 -0300

    Adicionar carrinho

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 function adicionar() {
     //Recuperar valores nome, quantidade e valor do produto
-    let produto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorProduto = produto.split('R$')[1];
-    let qtdProduto = document.getElementById('quantidade');
+    let qtdProduto = document.getElementById('quantidade').value;
    
-    let preco = qtdProduto.value * valorProduto;
     //Calcular o preço final
+    let preco = qtdProduto * valorProduto;
+    let carrinho= document.getElementById('lista-produtos');
+    //Adicionar no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${qtdProduto}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
     
 
-    //Adicionar no carrinho
     //Atualizar o valor total
 }
 
