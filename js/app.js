5ac8fa91867e450d3bff93d835de713c4e6cbb06
roysonnovo <royson.novo@gmail.com>
2025-09-03 09:26:29 -0300

    Recuperar valores

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function adicionar() {
+    //Recuperar valores nome, quantidade e valor do produto
+    let nomeProduto = document.getElementById('produto');
+    let qtdProduto = document.getElementById('quantidade');
+    
+    //Calcular o preço
+    //Adicionar no carrinho
+    //Atualizar o valor total
+}
+
+function limpar () {
+
+}
