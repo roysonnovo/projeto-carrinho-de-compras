784e4e8d075a283c79cb9e409ce600a8ac9922bd
roysonnovo <royson.novo@gmail.com>
2025-09-03 10:06:25 -0300

    Validar dados

--- js/app.js
+++ js/app.js
@@ -7,14 +7,29 @@ function adicionar() {
     let nomeProduto = produto.split('-')[0];
     let valorProduto = produto.split('R$')[1];
     let qtdProduto = document.getElementById('quantidade').value;
-   //Calcular o preço final
+    
+    // Verificar se o produto selecionado é válido
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+
+    // Verificar se a quantidade inserida é válida
+    if (isNaN(qtdProduto) || qtdProduto <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+
+    //Calcular o preço final
     let preco = qtdProduto * valorProduto;
     let carrinho= document.getElementById('lista-produtos');
+    
     //Adicionar no carrinho
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${qtdProduto}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`;
-    //Atualizar o valor total
+    
+        //Atualizar o valor total
     totalGeral = totalGeral + preco;
     let valorTotal = document.getElementById('valor-total');
     valorTotal.textContent = `R$ ${totalGeral}`;
